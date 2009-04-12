a8d56eacd83c77bdf591c24ae0c7d2c3c7ab418d
yuanhao <yuanhao@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-12 19:01:32 +0000

    uploading 0.2
    git-svn-id: https://plugins.svn.wordpress.org/japansoc-voting-button-for-blogs/tags/0.2@110053 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ japansoc.php
@@ -0,0 +1,124 @@
+<?php 
+/*
+Plugin Name: JapanSoc: Soc it!
+Plugin URI: http://www.japansoc.com
+Description: Allows your users to submit your posts to JapanSoc.com and vote.
+Version: 0.2
+Author: Nick Ramsay (thanks to Louis Ponder)
+Author URI: http://www.japansoc.com
+*/
+
+$japansocsite = "http://www.japansoc.com/evb/button.php";
+$float_button = "left"; 
+$button_margin_left = 0;   
+$button_margin_right = 10;
+
+$float_button2 = "right"; 
+$button_margin_left2 = 10;   
+$button_margin_right2 = 0; 
+
+function add_japansoc()
+{
+	global $post, $japansocsite, $float_button, $button_margin_left, $button_margin_right;
+	$middle = get_post_meta($post->ID, 'japansocleft', $single=true);
+	if ($middle != ''){
+		$before = "<span style=\"float:".$float_button.";\"><script type=\"text/javascript\" src=\"".$japansocsite."\">";
+		$end = "';</script></span>";
+		return $before.$middle.$end;
+	}
+	else{
+		if(!is_search() && !is_archive() && !is_category() && !is_tag()) 
+			{
+				$script_bit = "<script type=\"text/javascript\"> submit_url = \"" . get_permalink($post->ID) . "\"; </script>";
+			}
+		else
+			{
+				$script_bit = "";
+			}
+		return "<span style=\"padding-top: 0px; padding-bottom: 2px; margin-left:".$button_margin_left."px; margin-right:".$button_margin_right."px; float:".$float_button.";\">".$script_bit."<script type=\"text/javascript\" src=\"".$japansocsite."\"></script></span>";
+	}
+}
+
+function add_japansoc2()
+{
+	global $post,$japansocsite,$float_button2,$button_margin_left2,$button_margin_right2;
+	$middle = get_post_meta($post->ID, 'japansocright', $single=true);
+	if ($middle != ''){
+		$before = "<span style=\"float:".$float_button2.";\"><script type=\"text/javascript\" src=\"".$japansocsite."\">";
+		$end = "';</script></span>";
+		return $before.$middle.$end;
+	}
+	else{
+		if(!is_search() && !is_archive() && !is_category() && !is_tag()) 
+			{
+				$script_bit = "<script type=\"text/javascript\"> submit_url = \"" . get_permalink($post->ID) . "\"; </script>";
+			}
+		else
+			{
+				$script_bit = "";
+			}
+		return "<span style=\"padding-top: 0px; padding-bottom: 2px; margin-left:".$button_margin_left2."px; margin-right:".$button_margin_right2."px; float:".$float_button2.";\">".$script_bit."<script type=\"text/javascript\" src=\"".$japansocsite."\"></script></span>";
+	}
+}
+
+function add_japansoc_quicktag()
+{
+	if(strpos($_SERVER['REQUEST_URI'], 'post.php'))
+	{
+		?>
+		<script language="JavaScript" type="text/javascript"><!--
+		var toolbar = document.getElementById("ed_toolbar");
+		<?php
+				edit_insert_button("JSoc-L", "japansocleft", "japansocleft");
+				edit_insert_button("JSoc-R", "japansocright", "japansocright");
+		?>
+		function japansocleft()
+		{
+			edInsertContent(edCanvas, '\<!--japansocleft--\>');
+		}
+		function japansocright()
+		{
+			edInsertContent(edCanvas, '\<!--japansocright--\>');
+		}
+
+		//--></script>
+		<?php
+	}
+}
+if(!function_exists('edit_insert_button'))
+{
+	function edit_insert_button($caption, $js_onclick, $title = '')
+	{
+		?>
+		if(toolbar)
+		{
+			var theButton = document.createElement('input');
+			theButton.type = 'button';
+			theButton.value = '<?php echo $caption; ?>';
+			theButton.onclick = <?php echo $js_onclick; ?>;
+			theButton.className = 'ed_button';
+			theButton.title = "<?php echo $title; ?>";
+			theButton.id = "<?php echo "ed_{$caption}"; ?>";
+			toolbar.appendChild(theButton);
+		}
+		<?php
+	}
+}
+function japansoc_button_check($content)
+{
+	if(preg_match('|<!--japansoc-->|', $content)){
+		return str_replace('<!--japansoc-->', add_japansoc(), $content);
+	}
+	elseif(preg_match('|<!--japansocleft-->|', $content)){
+		return str_replace('<!--japansocleft-->', add_japansoc(), $content);
+	}
+	elseif(preg_match('|<!--japansocright-->|', $content)){
+		return str_replace('<!--japansocright-->', add_japansoc2(), $content);
+	}
+	else {
+		return $content;	
+	}
+}
+add_filter('admin_footer', 'add_japansoc_quicktag');
+add_action('the_content', 'japansoc_button_check');
+?>
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== JapanSoc.com Soc it! Plugin for Wordpress ===
+Contributors: yuanhao
+Donate link: http://faq.japansoc.com/japansoc/sponsors
+Tags: japansoc, soc it, social bookmarking
+Requires at least: 2.0
+Tested up to: 2.7.1
+Stable tag: 0.2
+
+Allows your users to submit and vote for your posts on JapanSoc.com.
+
+== Description ==
+
+
+= Usage: =
+
+* Insert either "&lt;!--japansocleft--&gt;" or "&lt;!--japansocright--&gt;" (without quotes) into your posts where you want the
+button to appear using the HTML editor.
+* Alternatively, you can use the HTML editor buttons labeled **JSoc-L** and **JSoc-R**
+
+= Notes: =
+
+1. This feature won't work in the Rich Text Editor. You have to use the HTML editor.
+
+1. The difference between 0.2 and the previous 0.1 version of this plugin is 
+the left/right alignment option directly in your post. The tag <!--japansoc-->" used in version 0.1 still works and defaults to the left.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload the folder "japansoc" to your /wp-content/plugins/ folder
+2. Go to the Plugins section on your dashboard and activate the plugin "JapanSoc: Soc it!"
+
+== Screenshots ==
+
+1. screenshot-1.(png|jpg|jpeg|gif).
+1. screenshot-2.(png|jpg|jpeg|gif).
+
+== Credits ==
+
+I did not write this plugin, I am merely a contributor! Original credit for this plugin goes to:
+
+* Original design of the wordpress plugin: Andrew Egenes [www.designfloat.com](http://www.designfloat.com)
+* Edited originally by Louis Ponder of [www.otakustuff.com](http://www.otakustuff.com)
+* Conversion to JapanSoc by Nick Ramsay [www.japansoc.com](http://www.japansoc.com)
+* New button and WP Plugin Codex upload by Yuan-Hao Chiang [www.instant-ramen.net](http://www.instant-ramen.net)
